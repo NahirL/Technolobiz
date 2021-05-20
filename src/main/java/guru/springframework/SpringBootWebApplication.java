10f360c2e7060a06847ad1ca960d996a13f4b8ac
Nahir <leiva_nahir@hotmail.com>
2021-05-20 17:51:26 -0300

    Modificacion de prueba

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -10,4 +10,5 @@ public class SpringBootWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+    //Esta es una prueba para Git
 }
